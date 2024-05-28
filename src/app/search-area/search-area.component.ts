c1c7b82218a05b1d11e187255f7b5e8338bf9b74
Leandro Sousa <151054241+sousalmdev@users.noreply.github.com>
2024-05-28 07:07:43 -0300

    Update search-area.component.ts

--- src/app/search-area/search-area.component.ts
+++ src/app/search-area/search-area.component.ts
@@ -1,34 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {MatSelectChange, MatSelectModule, MatSelectTrigger} from '@angular/material/select';
-
-import * as data from 'src/api/api.service';
-import { MatMenu } from '@angular/material/menu';
-import { MatIcon } from '@angular/material/icon';
-import { MatIconButton } from '@angular/material/button';
+import { MatSelectChange } from '@angular/material/select';
+import { ApiService } from 'src/api/api.service';
 
 @Component({
   selector: 'app-search-area',
   templateUrl: './search-area.component.html',
 })
 export class SearchAreaComponent implements OnInit {
-  allCars: any = (data as any);
+  allCars: any[] = [];
   displayedCars: any[] = [];
   query: string = '';
   sortCriteria: 'anoAsc' | 'anoDesc' | 'marcaAsc' = 'marcaAsc';
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private apiService: ApiService
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.query = params['query'];
-      this.filterCars();
+    this.apiService.getItems().subscribe(cars => {
+      this.allCars = cars;
+      this.route.params.subscribe(params => {
+        this.query = params['query'];
+        this.filterCars();
+      });
     });
   }
 
   filterCars(): void {
     if (this.query) {
-      this.displayedCars = this.allCars.filter((car: any) =>
+      this.displayedCars = this.allCars.filter(car =>
         car.marca.toLowerCase().includes(this.query.toLowerCase()) ||
         car.modelo.toLowerCase().includes(this.query.toLowerCase())
       );
@@ -42,27 +45,10 @@ export class SearchAreaComponent implements OnInit {
   sortCars(): void {
     this.displayedCars.sort((a, b) => {
       if (this.sortCriteria === 'anoDesc') {
-        return a.ano - b.ano;
-      } else if (this.sortCriteria === 'anoAsc') {
         return b.ano - a.ano;
+      } else if (this.sortCriteria === 'anoAsc') {
+        return a.ano - b.ano;
       } else if (this.sortCriteria === 'marcaAsc') {
         return a.marca.localeCompare(b.marca);
       }
       return 0;
-    });
-  }
-
-
-
-  onSortChange(event: MatSelectChange): void {
-    const value = event.value;
-    if (value) {
-      this.sortCriteria = value as 'anoAsc' | 'anoDesc' | 'marcaAsc'
-      this.sortCars();
-    }
-  }
-
-  navToDetails(id: number): void {
-    this.router.navigateByUrl(`home/details/${id}`);
-  }
-}
